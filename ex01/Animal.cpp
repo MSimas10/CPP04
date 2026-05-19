6fc8cd3f3970c3bbb3ffcb6a274ce6c95fce1f2a
Miroslav Simek <simek.miroslav@email.cz>
2026-05-19 19:31:00 +0200

    Ex01 in progress

--- ex01/Animal.cpp
+++ ex01/Animal.cpp
@@ -10,3 +10,28 @@ Animal::Animal(const Animal& other)
     std::cout << "Animal copy constructor called" <<std::endl; 
     *this = other;
 }
+Animal& Animal::operator=(const Animal& other)
+{
+    std::cout << "Animal copy assignment operator called" << std::endl;
+    if(this != &other)
+    {
+        this -> _type = other._type;
+
+    }
+    return *this;
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal constructor was destroyed" << std::endl;
+}
+
+void Animal::makeSound(void) const
+{
+    std::cout << "Animal random sound" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+    return this-> _type;
+}
---
+++ ex01/Cat.cpp
@@ -0,0 +1,33 @@
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+    std::cout << "Cat default constructor called" << std::endl;
+    this-> _type = "Cat";
+}
+
+Cat::Cat(const Cat& other)
+{
+    std::cout << "Cat copy constructor called" << std::endl;
+    *this = other;
+}
+
+Cat& Cat::operator=(const Cat& other)
+{
+    if(this != &other)
+    {
+        _type = other._type;
+    }
+    std::cout << "Cat asignment operator called." << std::endl;
+    return *this;
+}
+
+Cat::~Cat()
+{
+    std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+    std::cout << "Mnau Mnau" << std::endl;
+}
